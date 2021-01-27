9c1ff11f5a7100f990065b19524a2d074e8dcf69
Vitor Garcia <vitor.stabile@hotmail.com>
2021-01-26 21:40:07 -0300

    Push Sequencial Structure Class

---
+++ class15-data-processing-casting/src/Main.java
@@ -0,0 +1,58 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x, y;
+		
+		x = 5;
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		int x2;
+		
+		double y2;
+		
+		x2 = 5;
+		y2 = 2 * x2;
+		
+		System.out.println(x2);
+		System.out.println(y2);
+		
+		double b, B, h, area;
+		
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		
+		area = (b + B) / 2.0 * h;
+		System.out.println(area);
+		
+		int x3, y3;
+		double resultado;
+		
+		x3 = 5;
+		y3 = 2;
+		resultado = x3 / y3;
+		
+		System.out.println(resultado);
+		
+		resultado = (double) x3 / y3;
+		
+		System.out.println(resultado);
+		
+		double x4;
+		int y4;
+		
+		x4 = 5.0;
+		y4 = (int) x4;
+		
+		System.out.println(y4);
+		
+		
+
+	}
+
+}
